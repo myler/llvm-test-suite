71d2eebf54afc4062db088aeaf1c6e54bab849bd
Dounia Khaldi <dounia.khaldi@intel.com>
2022-01-07 17:20:10 -0600

    [SYCL][Matrix] test the two features: fill a matrix and element wise operations (#645)
    Two new matrix features are being added to the DPC++ compiler namely: fill a matrix and element wise operations. This PR adds tests for these two features.
    
    Signed-off-by: Dounia Khaldi dounia.khaldi@intel.com

--- SYCL/Matrix/element_wise_all_ops_half.cpp
+++ SYCL/Matrix/element_wise_all_ops_half.cpp
@@ -44,6 +44,10 @@ template <typename T, size_t M, size_t N>
 void assert_ops_ref(
     accessor<T, 2, access::mode::read, access::target::host_buffer> C,
     const float ref) {
+void assert_ops_ref(/*const T &C*/ accessor<T, 2, access::mode::read,
+                                            access::target::host_buffer>
+                        C,
+                    const float ref) {
   for (size_t i = 0; i < M; i++)
     for (size_t j = 0; j < N; j++) {
       auto diff = C[i][j] - ref;
@@ -205,6 +209,7 @@ void matrix_verify_logic(queue q, big_matrix<T, M, N> &A, nd_range<2> &r,
                wi_slice_a[i] < 2.0 || wi_slice_a[i] <= 2.0) {
              T val = (wi_slice_a[i] != 2.0) ? wi_slice_a[i]
                                             : static_cast<half>(2.0);
+             T val = (wi_slice_a[i] != 2.0) ? wi_slice_a[i] : 2.0;
              val--;
              val++;
              if (wi_slice_a[i] == 2.0) {
